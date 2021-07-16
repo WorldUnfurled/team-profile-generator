d7fbada179eba8a22ab748c5c7652b1e1bd68919
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-16 09:25:26 -0400

    Created firstRole prompt in index which asks user to choose first role they want from list

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+inquirer
+    .prompt({
+        name: 'firstRole',
+        message: "Which role would you like to build first?",
+        type: 'list',
+        choices: ['Manager', 'Engineer', 'Intern']
+    })
