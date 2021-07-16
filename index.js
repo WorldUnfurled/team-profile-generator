2fc1505f6c997d40d616ea196371558294f4df2b
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-16 17:36:34 -0400

    Put main inquirer builder inside of builder variable

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt({
-        name: 'firstRole',
-        message: "Which role would you like to build first?",
-        type: 'list',
-        choices: ['Manager', 'Engineer', 'Intern']
-    })
-    .then((data) => {
+const builder = () => {
 
-        switch(data.firstRole) {
-            case 'Manager':
-                console.log('Manager');
-                break;
-            case 'Engineer':
-                console.log('Engineer');
-                break;
-            case 'Intern':
-                console.log('Intern');
-                break;
-            default:
-                console.log('Please enter a valid name');
-    }
+    inquirer
+        .prompt({
+            name: 'role',
+            message: "Which role would you like to build first?",
+            type: 'list',
+            choices: ['Manager', 'Engineer', 'Intern']
+        })
+        .then((data) => {
 
-    });
+            switch(data.role) {
+                case 'Manager':
+                    console.log('Manager');
+                    break;
+                case 'Engineer':
+                    console.log('Engineer');
+                    break;
+                case 'Intern':
+                    console.log('Intern');
+                    break;
+                default:
+                    console.log('Please enter a valid name');
+            }
+
+        });
+
+}
+
+builder();
