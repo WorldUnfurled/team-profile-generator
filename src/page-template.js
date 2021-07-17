d8add039a73251f1f5b8b0808bf6f2716f0da486
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-17 16:31:53 -0400

    Added navbar

--- src/page-template.js
+++ src/page-template.js
@@ -9,7 +9,10 @@ const generateTemplate = (memberCards) => {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>Document</title>
     </head>
-    <body>`
+    <body>
+    <nav class="navbar navbar-light bg-light text-center">
+      <span class="navbar-brand mb-0 h1">Employees</span>
+    </nav>`
         + 
         memberCards
         +
